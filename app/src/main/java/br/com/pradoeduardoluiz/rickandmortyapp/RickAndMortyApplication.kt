6eb67d786458b3b8e9b632f6c1df474cb406469a
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2023-02-07 09:06:31 -0300

    add koin to dependency injection in project

---
+++ app/src/main/java/br/com/pradoeduardoluiz/rickandmortyapp/RickAndMortyApplication.kt
@@ -0,0 +1,22 @@
+package br.com.pradoeduardoluiz.rickandmortyapp
+
+import android.app.Application
+import br.com.pradoeduardoluiz.rickandmortyapp.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class RickAndMortyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@RickAndMortyApplication)
+            modules(modules)
+        }
+    }
+
+    private val modules = listOf(appModule)
+
+}
---
+++ app/src/main/java/br/com/pradoeduardoluiz/rickandmortyapp/di/AppModule.kt
@@ -0,0 +1,7 @@
+package br.com.pradoeduardoluiz.rickandmortyapp.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
